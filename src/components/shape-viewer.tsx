75970322233e5100974ac8269b078dea1d9dcb98
Mike Bannister <notimpossiblemike@gmail.com>
2025-06-21 14:33:46 -0400

    Add individual face rendering with configurable scale factor to ShapeViewer
    - Replace single indexed face set with individual face rendering
    - Add scaleFactor prop (default 0.8) to control face scaling from center
    - Calculate face centers and scale vertices relative to face center
    - Enable solid face rendering for better visual separation

--- src/components/shape-viewer.tsx
+++ src/components/shape-viewer.tsx
@@ -7,9 +7,14 @@ interface ShapeViewerProps {
   vertices: number[][]
   faces: number[][]
   edges: number[][]
+  scaleFactor?: number
 }
 
-export default function ShapeViewer({ vertices, faces }: ShapeViewerProps) {
+export default function ShapeViewer({
+  vertices,
+  faces,
+  scaleFactor = 0.8,
+}: ShapeViewerProps) {
   const containerRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
@@ -20,9 +25,18 @@ export default function ShapeViewer({ vertices, faces }: ShapeViewerProps) {
     })
   }, [])
 
-  const coordinatePoints = vertices.map(vertex => vertex.join(' ')).join(', ')
-
-  const faceIndices = faces.map(face => [...face, -1].join(' ')).join(' ')
+  const calculateFaceCenter = (face: number[]) => {
+    const faceVertices = face.map(index => vertices[index])
+    const center = faceVertices.reduce(
+      (acc, vertex) => [
+        acc[0] + vertex[0] / faceVertices.length,
+        acc[1] + vertex[1] / faceVertices.length,
+        acc[2] + vertex[2] / faceVertices.length,
+      ],
+      [0, 0, 0],
+    )
+    return center
+  }
 
   return (
     <div
@@ -36,17 +50,35 @@ export default function ShapeViewer({ vertices, faces }: ShapeViewerProps) {
             orientation='0 1 0 0'
             fieldOfView='0.785398'
           ></viewpoint>
-          <shape>
-            <appearance>
-              <material
-                emissiveColor='0.6 0.8 1'
-                diffuseColor='0 0 0'
-              ></material>
-            </appearance>
-            <indexedFaceSet solid='false' coordIndex={faceIndices}>
-              <coordinate point={coordinatePoints}></coordinate>
-            </indexedFaceSet>
-          </shape>
+          {faces.map((face, index) => {
+            const center = calculateFaceCenter(face)
+            const faceCoordinates = face
+              .map(vertexIndex => {
+                const vertex = vertices[vertexIndex]
+                const scaledVertex = [
+                  center[0] + (vertex[0] - center[0]) * scaleFactor,
+                  center[1] + (vertex[1] - center[1]) * scaleFactor,
+                  center[2] + (vertex[2] - center[2]) * scaleFactor,
+                ]
+                return scaledVertex.join(' ')
+              })
+              .join(', ')
+            const faceIndices = [...Array(face.length).keys(), -1].join(' ')
+
+            return (
+              <shape key={index}>
+                <appearance>
+                  <material
+                    emissiveColor='0.6 0.8 1'
+                    diffuseColor='0 0 0'
+                  ></material>
+                </appearance>
+                <indexedFaceSet solid='true' coordIndex={faceIndices}>
+                  <coordinate point={faceCoordinates}></coordinate>
+                </indexedFaceSet>
+              </shape>
+            )
+          })}
         </scene>
       </x3d>
     </div>

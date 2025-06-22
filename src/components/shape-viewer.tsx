74c11e128dd04dddc0b490b8bcb6d9046fbe35d4
Mike Bannister <notimpossiblemike@gmail.com>
2025-06-21 20:54:20 -0400

    fix shape viewer reload

--- src/components/shape-viewer.tsx
+++ src/components/shape-viewer.tsx
@@ -94,7 +94,7 @@ export default function ShapeViewer({
         }
       }
     })
-  }, [])
+  }, [vertices, faces])
 
   const x3dContent = `
     <x3d width="600px" height="600px" style="width: 100%; height: 100%; display: block;">
